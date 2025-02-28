77fbd78c2cbadb1a14479fc32bd5c54d762a0e3c
kantaGOTO <goto@cysec.cs.ritsumei.ac.jp>
2025-02-28 12:29:59 +0900

    add: comment

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,80 @@
+
 #![no_std]
 #![no_main]
+
+// バイト列をutf8へ変換
 use core::str::from_utf8;
 
-use cyw43::JoinOptions;
-use cyw43_pio::{PioSpi, DEFAULT_CLOCK_DIVIDER};
-use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::{DMA_CH0, PIO0, USB};
-use embassy_rp::pio::{InterruptHandler, Pio};
-use embassy_time::{Duration, Timer};
-use static_cell::StaticCell;
-
-use embassy_rp::usb::{Driver, InterruptHandler as usb_InterruptHandler};
+use embassy_rp::pio::{InterruptHandler as PioInterruptHandler, Pio};
+use embassy_rp::usb::{InterruptHandler as UsbInterruptHandler, Driver};
+use embassy_rp::clocks::RoscRng;
 
 use embassy_net::dns::DnsSocket;
 use embassy_net::tcp::client::{TcpClient, TcpClientState};
-use embassy_net::{tcp, Config, StackResources};
-use embassy_rp::clocks::RoscRng;
+use embassy_net::{Config, StackResources};
+
+use embassy_time::{Duration, Timer};
+use static_cell::StaticCell;
 use rand::RngCore;
-use reqwless::client::{HttpClient, TlsConfig, TlsVerify};
+
+use cyw43::JoinOptions;
+use cyw43_pio::{PioSpi, DEFAULT_CLOCK_DIVIDER};
+
+// 組み込み向けHTTPクライアント
+use reqwless::client::HttpClient;
 use reqwless::request::Method;
-use serde::Deserialize;
 
-use {defmt_rtt as _, panic_probe as _, serde_json_core};
+// logger
+use defmt::*;
+use {defmt_rtt as _, panic_probe as _};
 
+// WiFiのSSIDとパスワード
 const WIFI_SSID: &str = "SSID";
 const WIFI_PASSWORD: &str = "PASSWORD";
 
+// 接続先URL（HTTP限定）
+const TEST_URL: &str = "http://example.com";
+
+// 割り込みと割り込みハンドラを対応付け
 bind_interrupts!(struct Irqs {
-    PIO0_IRQ_0 => InterruptHandler<PIO0>;
-    USBCTRL_IRQ => usb_InterruptHandler<USB>;
+    PIO0_IRQ_0 => PioInterruptHandler<PIO0>;
+    USBCTRL_IRQ => UsbInterruptHandler<USB>;
 });
 
+// CYW43のドライバを実行するタスク
 #[embassy_executor::task]
 async fn cyw43_task(runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>) -> ! {
     runner.run().await
 }
 
+// ネットワークスタックのタスク
 #[embassy_executor::task]
 async fn net_task(mut runner: embassy_net::Runner<'static, cyw43::NetDriver<'static>>) -> ! {
     runner.run().await
 }
 
+// USBでのログ出力タスク
 #[embassy_executor::task]
 async fn logger_task(driver: Driver<'static, USB>) {
     embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
 }
 
+// エントリーポイント
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     // ペリフェラルを初期化
     let p = embassy_rp::init(Default::default());
 
+    // USBドライバを初期化, USBでログを出力するタスクの起動
     let driver = Driver::new(p.USB, Irqs);
-
-    let mut rng = RoscRng;
-
     spawner.spawn(logger_task(driver)).unwrap();
-    let mut counter = 0;
+    
+    // 乱数生成器を初期化
+    let mut rng = RoscRng;
 
     // WiFi ファームウェアとCLMを読み込む
     let fw = include_bytes!("../firmware/43439A0.bin");
@@ -83,31 +98,29 @@ async fn main(spawner: Spawner) {
         p.DMA_CH0,
     );
 
-    // WiFiチップの初期化
+    // WiFiチップのドライバを生成
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
     let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
     // WiFiチップのタスクを起動
     unwrap!(spawner.spawn(cyw43_task(runner)));
 
-
+    // WiFiチップの初期化
     control.init(clm).await;
-    control
-        .set_power_management(cyw43::PowerManagementMode::PowerSave)
-        .await;
+    control.set_power_management(cyw43::PowerManagementMode::PowerSave).await;
 
-    let delay = Duration::from_secs(1);
 
     let config = Config::dhcpv4(Default::default());
-
     let seed = rng.next_u64();
     
     static RESOURCES: StaticCell<StackResources<5>> = StaticCell::new();
     let (stack, runner) = embassy_net::new(net_device, config, RESOURCES.init(StackResources::new()), seed);
-
+    // ネットワークスタックのタスクを起動
     unwrap!(spawner.spawn(net_task(runner)));
 
+    // WiFiに接続
     loop {
+        // 接続に成功するまで繰り返す
         match control.join(WIFI_SSID, JoinOptions::new(WIFI_PASSWORD.as_bytes())).await {
             Ok(_) => break,
             Err(err) => {
@@ -143,11 +156,9 @@ async fn main(spawner: Spawner) {
 
         let mut http_client = HttpClient::new(&tcp_client, &dns_client);
 
-        let url = "http://example.com";
+        log::info!("connecting to {}", TEST_URL);
 
-        log::info!("connecting to {}", url);
-
-        let mut request = match http_client.request(Method::GET, &url).await {
+        let mut request = match http_client.request(Method::GET, &TEST_URL).await {
             Ok(req) => req,
             Err(e) => {
                 log::error!("Failed to make HTTP request: {:?}", e);
